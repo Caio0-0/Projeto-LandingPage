053bc747758639642c94a648c9471fb5446326f9
Caio0-0 <126476942+Caio0-0@users.noreply.github.com>
2023-05-01 08:50:24 -0300

    novo

---
+++ mobile.js
@@ -0,0 +1,15 @@
+function clicM(){
+    if (its.style.display == 'flex'){
+        its.style.display = 'none'
+    }else{
+        its.style.display = 'flex'
+    }
+}
+
+function mudouT(){
+    if(window.innerWidth >= 768){
+        its.style.display = 'flex'
+    }else{
+        its.style.display = 'none'
+    }
+}
